f7c14bec1f01ef89a7a27c64a15da71fd2a2209e
KushagraKatiyar06 <playkushagra@gmail.com>
2024-11-26 12:11:07 -0500

    kushi

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -30,7 +30,9 @@ class SudokuGenerator:
 
     def __init__(self, row_length, removed_cells):
         self.row_length = row_length
+        self.removed_cells = removed_cells
         self.board = []
+        self.box_length = math.pow(row_length, 1/2)
 
         return None
 
